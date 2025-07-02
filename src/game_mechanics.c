7ab133969ccc62e5f3555d566337c2835750c3c6
sojala <sojala@student.hive.fi>
2025-07-02 16:08:49 +0300

    Added new versions of game mechanics and render, will get back to main to get other files from Karo

--- src/game_mechanics.c
+++ src/game_mechanics.c
@@ -3,42 +3,177 @@
 /*                                                        :::      ::::::::   */
 /*   game_mechanics.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: khiidenh <khiidenh@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: sojala <sojala@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 18:07:53 by khiidenh          #+#    #+#             */
-/*   Updated: 2025/06/16 12:53:41 by khiidenh         ###   ########.fr       */
+/*   Updated: 2025/07/02 15:28:49 by sojala           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "so_long.h"
+#include "cub3D.h"
 
-static void	move(t_game *game, int y, int x)
+/*This function rotates "the player", right now it's reflected only on minimap*/
+static void	rotate(t_game *game, double rotation_dir)
 {
-//	printf("Old y: %d, old x: %d\n", game->player.y, game->player.x);
-	game->player.y = game->player.y + y;
-	game->player.x = game->player.x + x;
-//	printf("New y: %d, new x: %d\n", game->player.y, game->player.x);
-	game->images[PLAYER]->instances[0].y = game->player.y * TILE;
-	game->images[PLAYER]->instances[0].x = game->player.x * TILE;
+	double	rotspeed;
+	double	old_dir_x;
+	double	old_dir_y;
+
+	printf("Before rotation: dir_x=%f, dir_y=%f\n", game->player.dir_x, game->player.dir_y);
+	rotspeed = SPEED * rotation_dir;
+	old_dir_x = game->player.dir_x;
+	old_dir_y = game->player.dir_y;
+	game->player.dir_x = old_dir_x * cos(rotspeed) - old_dir_y * sin(rotspeed);
+	game->player.dir_y = old_dir_x * sin(rotspeed) + old_dir_y * cos(rotspeed);
+	printf("After rotation: dir_x=%f, dir_y=%f\n", game->player.dir_x, game->player.dir_y);
+	render_minimap(game);
 }
 
+/*This function is responsible of moving the player in the maze. Right now it'set up in
+a way that it moves relative to the player's facing direction (so forward doesnt always mean up etc)
+The old version is commented out in the end of the file*/
+static void	move(t_game *game, enum e_directions direction)
+{
+	double	old_y;
+	double	old_x;
+
+	old_y = game->player.y;
+	old_x = game->player.x;
+	printf("Old y: %f, old x: %f\n", game->player.y, game->player.x);
+	printf("Olds y: %d, old x: %d\n", (int)game->player.y, (int)game->player.x);
+	if (direction == FORWARD)
+	{
+	game->player.y = game->player.y + (SPEED * game->player.dir_y);
+	game->player.x = game->player.x + (SPEED * game->player.dir_x);
+	}
+	if (direction == BACKWARD)
+	{
+	game->player.y = game->player.y - (SPEED * game->player.dir_y);
+	game->player.x = game->player.x - (SPEED * game->player.dir_x);
+	}
+	if (direction == LEFT)
+	{
+	game->player.y = game->player.y - (SPEED * game->player.dir_x);
+	game->player.x = game->player.x + (SPEED * game->player.dir_y);
+	}
+	if (direction == RIGHT)
+	{
+	game->player.y = game->player.y + (SPEED * game->player.dir_x);
+	game->player.x = game->player.x - (SPEED * game->player.dir_y);
+	}
+	if (game->map[(int)game->player.y][(int)game->player.x] == '1')
+	{
+		game->player.y = old_y;
+		game->player.x = old_x;
+		return ;
+	}
+	printf("New y: %f, new x: %f\n", game->player.y, game->player.x);
+	printf("News y: %d, old x: %d\n", (int)game->player.y, (int)game->player.x);
+	render_minimap(game);
+}
+
+//Not sure if this is needed longer but keeping it here just in case
 void	key_hook(mlx_key_data_t keydata, t_game *game)
 {
-	if (keydata.action == 1 || keydata.action == 2)
-	{
-		if ((keydata.key == MLX_KEY_W || keydata.key == MLX_KEY_UP)
-			&& game->map[game->player.y - 1][game->player.x] != '1')
-			move(game, -1, 0);
-		if ((keydata.key == MLX_KEY_S || keydata.key == MLX_KEY_DOWN)
-			&& game->map[game->player.y + 1][game->player.x] != '1')
-			move(game, 1, 0);
-		if ((keydata.key == MLX_KEY_A || keydata.key == MLX_KEY_LEFT)
-			&& game->map[game->player.y][game->player.x - 1] != '1')
-			move(game, 0, -1);
-		if ((keydata.key == MLX_KEY_D || keydata.key == MLX_KEY_RIGHT)
-			&& game->map[game->player.y][game->player.x + 1] != '1')
-			move(game, 0, 1);
-		if (keydata.key == MLX_KEY_ESCAPE)
-			mlx_close_window(game->mlx);
-	}
 }
+
+/*
+"The WASD keys must allow you to move the point of view through the maze.
+The left and right arrow keys must allow you to look left and right in the maze.
+ESC must lose the window"
+*/
+void	loop_hook(void *param)
+{
+	t_game	*game;
+
+	game = param;
+	if (mlx_is_key_down(game->mlx, MLX_KEY_W))
+		move(game, FORWARD);
+	if (mlx_is_key_down(game->mlx, MLX_KEY_S))
+		move(game, BACKWARD);
+	if (mlx_is_key_down(game->mlx, MLX_KEY_A))
+		move(game, LEFT);
+	if (mlx_is_key_down(game->mlx, MLX_KEY_D))
+		move(game, RIGHT);
+	if (mlx_is_key_down(game->mlx, MLX_KEY_LEFT))
+		rotate(game, -1);
+	if (mlx_is_key_down(game->mlx, MLX_KEY_RIGHT))
+		rotate(game, 1);
+	if (mlx_is_key_down(game->mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(game->mlx);
+}
+
+/*THIS IS THE OLD VERSION.
+It handles the rotation and the movement but the movement does not follow players direction,
+so even if you are facing a certain way, pressing S moves player towards bottom of the map.*/
+
+// /*This function rotates "the player", right now it's reflected only on minimap*/
+// static void	rotate(t_game *game, double speed, double rotation_dir)
+// {
+// 	double	rotspeed;
+// 	double	old_dir_x;
+// 	double	old_dir_y;
+
+// 	printf("Before rotation: dir_x=%f, dir_y=%f\n", game->player.dir_x, game->player.dir_y);
+// 	rotspeed = speed * rotation_dir;
+// 	old_dir_x = game->player.dir_x;
+// 	old_dir_y = game->player.dir_y;
+// 	game->player.dir_x = old_dir_x * cos(rotspeed) - old_dir_y * sin(rotspeed);
+// 	game->player.dir_y = old_dir_x * sin(rotspeed) + old_dir_y * cos(rotspeed);
+// 	printf("After rotation: dir_x=%f, dir_y=%f\n", game->player.dir_x, game->player.dir_y);
+// 	render_minimap(game);
+// }
+
+// /*This function is responsible of moving the player in the maze. However
+// the player should probably move according to the direction that they are facing xd*/
+// static void	move(t_game *game, double y, double x)
+// {
+// 	double	new_y;
+// 	double	new_x;
+
+// 	new_y = game->player.y + y;
+// 	new_x = game->player.x + x;
+// 	if (game->map[(int)new_y][(int)new_x] == '1')
+// 	{
+// 		printf("Are we actually encountering blocker\n");
+// 		return ;
+// 	}
+// 	printf("Old y: %f, old x: %f\n", game->player.y, game->player.x);
+// 	printf("Olds y: %d, old x: %d\n", (int)game->player.y, (int)game->player.x);
+// 	game->player.y = game->player.y + y;
+// 	game->player.x = game->player.x + x;
+// 	printf("New y: %f, new x: %f\n", game->player.y, game->player.x);
+// 	printf("News y: %d, old x: %d\n", (int)game->player.y, (int)game->player.x);
+// 	render_minimap(game);
+// }
+
+// //Not sure if this is needed longer but keeping it here just in case
+// void	key_hook(mlx_key_data_t keydata, t_game *game)
+// {
+// }
+
+// /*
+// "The WASD keys must allow you to move the point of view through the maze.
+// The left and right arrow keys must allow you to look left and right in the maze.
+// ESC must lose the window"
+// */
+// void	loop_hook(void *param)
+// {
+// 	t_game	*game;
+
+// 	game = param;
+// 	if (mlx_is_key_down(game->mlx, MLX_KEY_W))
+// 		move(game, -0.1, 0);
+// 	if (mlx_is_key_down(game->mlx, MLX_KEY_S))
+// 		move(game, 0.1, 0);
+// 	if (mlx_is_key_down(game->mlx, MLX_KEY_A))
+// 		move(game, 0, -0.1);
+// 	if (mlx_is_key_down(game->mlx, MLX_KEY_D))
+// 		move(game, 0, 0.1);
+// 	if (mlx_is_key_down(game->mlx, MLX_KEY_LEFT))
+// 		rotate(game, 0.1, -1);
+// 	if (mlx_is_key_down(game->mlx, MLX_KEY_RIGHT))
+// 		rotate(game, 0.1, 1);
+// 	if (mlx_is_key_down(game->mlx, MLX_KEY_ESCAPE))
+// 		mlx_close_window(game->mlx);
+// }
--- src/render.c
+++ src/render.c
@@ -3,44 +3,275 @@
 /*                                                        :::      ::::::::   */
 /*   render.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: khiidenh <khiidenh@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: sojala <sojala@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 16:46:14 by khiidenh          #+#    #+#             */
-/*   Updated: 2025/06/16 11:25:06 by khiidenh         ###   ########.fr       */
+/*   Updated: 2025/07/02 15:27:26 by sojala           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "so_long.h"
+#include "cub3D.h"
 
-static void	draw_asset(t_game *game, enum e_assets type, int x, int y)
+/*
+--11--
+This function "combines" the different rgb values together into one integer.
+By shifting r 24 bits left, g 16 bits left, g 8 bits left and leaving a (alpha channel)
+where it is.
+*/
+static int	get_colour(int *rgb)
 {
-	if (type == BASE)
+	int	colour;
+	int	a;
+
+	a = 255;
+	colour = rgb[0] << 24 | rgb[1] << 16 | rgb[2] << 8 | a;
+	return (colour);
+}
+
+//The number 20 here represents the length of the line
+static void	draw_line(t_game *game, double beginX, double beginY)
+{
+	double	end_x;
+	double	end_y;
+	double	delta_x;
+	double	delta_y;
+	int		pixels;
+
+	end_x = beginX + game->player.dir_x * 20;
+	end_y = beginY + game->player.dir_y * 20;
+	delta_x = end_x - beginX;
+	delta_y = end_y - beginY;
+	pixels = sqrt((delta_x * delta_x) + (delta_y * delta_y));
+	delta_x /= pixels;
+	delta_y /= pixels;
+	while (pixels)
 	{
-	if (mlx_image_to_window(game->mlx, game->images[BASE],
-			x * TILE, y * TILE) < 0)
-		cleanup_and_exit(game, ERRIMG, 0);
-	}
-	if (type == WALL)
-	{
-		if (mlx_image_to_window(game->mlx, game->images[type],
-				x * TILE, y * TILE) < 0)
-			cleanup_and_exit(game, ERRIMG, 0);
-	}
-	if (type == EMPTY)
-	{
-		if (mlx_image_to_window(game->mlx, game->images[type],
-				x * TILE, y * TILE) < 0)
-			cleanup_and_exit(game, ERRIMG, 0);
-	}
-	if (type == PLAYER)
-	{
-		if (mlx_image_to_window(game->mlx, game->images[PLAYER],
-					game->player.x * TILE, game->player.y * TILE) < 0)
-			cleanup_and_exit(game, ERRIMG, 0);
+		mlx_put_pixel(game->minimapimage, beginX, beginY, 0x000099FF);
+		beginX += delta_x;
+		beginY += delta_y;
+		--pixels;
 	}
 }
 
+//There seems to be a bug with colours
+static void	draw_pixels(t_game *game, enum e_assets type, int x, int y)
+{
+	int	y_tile;
+	int	x_tile;
+	int	original_x;
+
+	y_tile = 0;
+	original_x = x;
+	while (y_tile < TILE)
+	{
+		x_tile = 0;
+		x = original_x;
+		while (x_tile < TILE)
+		{
+			if (type == BASE)
+				mlx_put_pixel(game->minimapimage, x, y, 0x00FFFFFF);
+			if (type == WALL)
+				mlx_put_pixel(game->minimapimage, x, y, 0x00CC00FF);
+			if (type == PLAYER && x_tile > 5 && x_tile < 14 && y_tile > 5 && y_tile < 14)
+				mlx_put_pixel(game->minimapimage, x, y, 0x009933FF);
+			x_tile++;
+			x++;
+		}
+		y++;
+		y_tile++;
+	}
+}
+
+/*
+--10--
+This function right now goes through the height and width of the map and puts pixel by pixel to the image.
+After we have filled the image, we put image to window.
+
 static void	render_map(t_game *game)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	y = 0;
+	while (y < MAX_SCREEN_HEIGHT / 2)
+	{
+		x = 0;
+		while (x < MAX_SCREEN_WIDTH)
+		{
+			mlx_put_pixel(game->image, x, y, get_colour(game->ceiling_rgb));
+			x++;
+		}
+		y++;
+	}
+	mlx_image_to_window(game->mlx, game->image, 0, 0);
+	y = MAX_SCREEN_HEIGHT / 2;
+	while (y < MAX_SCREEN_HEIGHT)
+	{
+		x = 0;
+		while (x < MAX_SCREEN_WIDTH)
+		{
+			mlx_put_pixel(game->image, x, y, get_colour(game->floor_rgb));
+			x++;
+		}
+		y++;
+	}
+	mlx_image_to_window(game->mlx, game->image, 0, 0);
+}*/
+
+void	draw_line_wall(void *image, int beginX, int beginY, int endX, int endY, int color)
+{
+	double	deltaX = endX - beginX;	//length of line on x axis
+	double	deltaY = endY - beginY;	//lengths of line on y axis
+	int		pixels = sqrt((deltaX * deltaX) + (deltaY * deltaY));
+	//pythagorean !! so pixels gets the "length of the hypotenuse"
+
+	deltaX /= pixels;	//we want to increment pixelX by "one pixel" and not the whole length
+	deltaY /= pixels;
+
+	double	pixelX = beginX;
+	double	pixelY = beginY;
+	while (pixels)
+	{
+		mlx_put_pixel(image, pixelX, pixelY, color);
+		pixelX += deltaX;
+		pixelY += deltaY;
+		pixels--;
+	}
+}
+
+void	render_map(t_game *game)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	y = 0;
+	while (y < MAX_SCREEN_HEIGHT / 2)
+	{
+		x = 0;
+		while (x < MAX_SCREEN_WIDTH)
+		{
+			mlx_put_pixel(game->image, x, y, get_colour(game->ceiling_rgb));
+			x++;
+		}
+		y++;
+	}
+	mlx_image_to_window(game->mlx, game->image, 0, 0);
+	y = MAX_SCREEN_HEIGHT / 2;
+	while (y < MAX_SCREEN_HEIGHT)
+	{
+		x = 0;
+		while (x < MAX_SCREEN_WIDTH)
+		{
+			mlx_put_pixel(game->image, x, y, get_colour(game->floor_rgb));
+			x++;
+		}
+		y++;
+	}
+	mlx_image_to_window(game->mlx, game->image, 0, 0);
+
+	double	posX = game->player.x;	//player position X
+	double	posY = game->player.y;	//player position Y
+	double	dirX = game->player.dir_x, dirY = game->player.dir_y;	//direction is along y-axis upwards (normalized so between -1 and 1)
+	double	planeX = 0.66;	// this is to make the field of view 66 degrees
+	double	planeY = 0; //planeY is 0 bc it has to be perpendicular
+	double	raydirY;
+	double	raydirX;
+	double	sidedistX;
+	double	sidedistY;
+	int		mapX, mapY;
+	int		stepX;
+	int		stepY;
+	int		hit = 0;
+	int		side;
+
+	x = 0;
+	while (x < MAX_SCREEN_WIDTH)
+	{
+		mapX = (int)posX;
+		mapY = (int)posY;
+		hit = 0;
+		side = 0;
+		double cameraX = 2.0 * (x + 0.5) / (double)MAX_SCREEN_WIDTH - 1.0;
+		raydirX = dirX + planeX * cameraX;	//position vector + specific part of camera plane
+		raydirY = dirY + planeY * cameraX;
+		double	deltadistX = (raydirX == 0) ? 1e30 : fabs(1 / raydirX);
+		double	deltadistY = (raydirY == 0) ? 1e30 : fabs(1 / raydirY);
+		double	perpwalldist;
+
+		if (raydirX < 0)	//if ray is moving left
+		{
+			stepX = -1;
+			sidedistX = (posX - mapX) * deltadistX;
+		}
+		else
+		{
+			stepX = 1;
+			sidedistX = (mapX + 1.0 - posX) * deltadistX;
+		}
+		if (raydirY < 0)	//if ray is moving up
+		{
+			stepY = -1;
+			sidedistY = (posY - mapY) * deltadistY;
+		}
+		else
+		{
+			stepY = 1;
+			sidedistY = (mapY + 1.0 - posY) * deltadistY;
+		}
+
+		while (hit == 0)
+		{
+			if (sidedistX < sidedistY)
+			{
+				sidedistX += deltadistX;
+				mapX += stepX;
+				side = 0;
+			}
+			else
+			{
+				sidedistY += deltadistY;
+				mapY += stepY;
+				side = 1;
+			}
+			if (game->map[mapY][mapX] == 1)
+				hit = 1;
+		}
+		if (side == 0)
+		{
+			perpwalldist = (sidedistX - deltadistX);
+		}
+		else
+		{
+			perpwalldist = (sidedistY - deltadistY);
+		}
+		//This section draws the '3d' view but it is veeeery initial version
+		//  Calculate height of line to draw on screen
+    	double lineHeight = (MAX_SCREEN_HEIGHT / perpwalldist);
+      	//calculate lowest and highest pixel to fill in current stripe
+     	int drawStart = -lineHeight / 2 + MAX_SCREEN_HEIGHT / 2;
+		if(drawStart < 0)drawStart = 0;
+      	printf("drawStart %d\n", drawStart);
+      	int drawEnd = lineHeight / 2 + MAX_SCREEN_HEIGHT / 2;
+     	if(drawEnd >= MAX_SCREEN_HEIGHT)drawEnd = MAX_SCREEN_HEIGHT - 1;
+		printf("drawEnd %d\n", drawEnd);
+		int l = 0;
+		while (l < TILE)
+		{
+			draw_line_wall(game->image, x + l, drawStart, x + l, drawEnd, 0xFF0000);
+			l++;
+		}
+		x++;
+	}
+}
+
+/*
+--9--
+This is basically just rendering the minimap on the top left corner, we can remove it if we want! :D
+The function also calls to the render actual map which right now adds the floor and ceiling colors.
+*/
+void	render_minimap(t_game *game)
 {
 	int	x;
 	int	y;
@@ -49,43 +280,44 @@ static void	render_map(t_game *game)
 	while (game->map[y] != NULL)
 	{
 		x = 0;
-		while (x <= (int)ft_strlen(game->map[y]))
+		while (x < (int)ft_strlen(game->map[y]))
 		{
 			if (game->map[y][x] == '1')
-				draw_asset(game, WALL, x, y);
-			else if (game->map[y][x] == ' ')
-				draw_asset(game, EMPTY, x, y);
-			else
-				draw_asset(game, BASE, x, y);
+				draw_pixels(game, WALL, x * TILE, y * TILE);
+			else if (ft_strchr("0NSEW", game->map[y][x]))
+			{
+				draw_pixels(game, BASE, x * TILE, y * TILE);
+			}
 			x++;
 		}
 		y++;
 	}
-	draw_asset(game, PLAYER, x, y);
+	draw_pixels(game, PLAYER, (game->player.x - 0.5) * TILE, (game->player.y - 0.5) * TILE);
+	draw_line(game, game->player.x * TILE, game->player.y * TILE);
+	if (mlx_image_to_window(game->mlx, game->minimapimage,
+			0, 0) < 0)
+		cleanup_and_exit(game, ERRIMG, 0);
 }
 
-static const char	**get_asset_paths(void)
-{
-	static const char	*asset_paths[ASSET_COUNT];
-
-	asset_paths[0] = "assets/base.png";
-	asset_paths[1] = "assets/wall.png";
-	asset_paths[2] = "assets/player_left.png";
-	asset_paths[3] = "assets/exit_open.png";
-	return (asset_paths);
-}
-
+/*
+--8--
+Here we are creating a new image that we will use for drawing floor and ceiling.
+We are also loading the wall textures and putting the textures to image.
+In addition, we are creating an image for the minimap.
+After this, we "render map"
+*/
 void	load_textures(t_game *game)
 {
 	int				i;
 	mlx_texture_t	*texture;
 	const char		**asset_paths;
 
-	asset_paths = get_asset_paths();
+	game->image = mlx_new_image(game->mlx, MAX_SCREEN_WIDTH, MAX_SCREEN_HEIGHT);
+	game->minimapimage = mlx_new_image(game->mlx, game->width * TILE, game->height * TILE);
 	i = 0;
 	while (i < ASSET_COUNT)
 	{
-		texture = mlx_load_png(asset_paths[i]);
+		texture = mlx_load_png(game->asset_paths[i]);
 		if (texture == NULL)
 			cleanup_and_exit(game, ERRPNG, 0);
 		game->images[i] = mlx_texture_to_image(game->mlx, texture);
@@ -100,4 +332,5 @@ void	load_textures(t_game *game)
 		i++;
 	}
 	render_map(game);
+	render_minimap(game);
 }

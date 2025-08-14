5786e5f90dea03b7d010b49bd907d04909e460c9
sojala <sojala@student.hive.fi>
2025-08-14 11:42:49 +0300

    Fixed minimap so that camera correctly follows player. Couldnt make it work to not draw over the edge wall so theres now excess black wall at the bottom and right end, is it ok?

--- src/render_minimap.c
+++ src/render_minimap.c
@@ -1,5 +1,50 @@
 #include "../include/cub3D.h"
 
+/*This function calculates the starting and end points of the portion
+of the minimap that we render to be visible at the moment, focusing on
+the player.*/
+static int	minimap_x_edge(double player, bool start, int max, int start_point)
+{
+	int	point;
+
+	if (start)
+	{
+		point = player - 10;
+		if (point < 0)
+			point = 0;
+	}
+	else
+	{
+		point = player + 10;
+		if (point - start_point < 20)
+			point += (20 - (point - start_point));
+		if (point >= max)
+			point = max;
+	}
+	return (point);
+}
+
+static int	minimap_y_edge(double player, int start, int max, int start_point)
+{
+	int	point;
+
+	if (start)
+	{
+		point = player - 6;
+		if (point < 0)
+			point = 0;
+	}
+	else
+	{
+		point = player + 6;
+		if (point - start_point < 12)
+			point += (12 - (point - start_point));
+		if (point >= max)
+			point = max;
+	}
+	return (point);
+}
+
 static void	draw_minimap_base(t_game *game, int x_start, int y_start, int y_end)
 {
 	int	y;
@@ -13,7 +58,7 @@ static void	draw_minimap_base(t_game *game, int x_start, int y_start, int y_end)
 	while (y < y_end)
 	{
 		x = x_start;
-		x_end = minimap_edge(game->player.x, false, ft_strlen(game->map[y]));
+		x_end = minimap_x_edge(game->player.x, false, ft_strlen(game->map[y]), x_start);
 		x1 = 0;
 		while (x < x_end)
 		{
@@ -29,37 +74,15 @@ static void	draw_minimap_base(t_game *game, int x_start, int y_start, int y_end)
 	}
 }
 
-/*This function calculates the starting and end points of the portion
-of the minimap that we render to be visible at the moment, focusing on
-the player.*/
-int	minimap_edge(double coordinate, bool start, int max)
-{
-	int	point;
-
-	if (start)
-	{
-		point = coordinate - 10;
-		if (point < 0)
-			point = 0;
-	}
-	else
-	{
-		point = coordinate + 10;
-		if (point >= max)
-			point = max;
-	}
-	return (point);
-}
-
 void	render_minimap(t_game *game)
 {
 	int	x_start;
 	int	y_start;
 	int	y_end;
 
-	x_start = minimap_edge(game->player.x, true, 0);
-	y_start = minimap_edge(game->player.y, true, 0);
-	y_end = minimap_edge(game->player.x, false, game->height);
+	x_start = minimap_x_edge(game->player.x, true, game->width, 0);
+	y_start = minimap_y_edge(game->player.y, true, game->height, 0);
+	y_end = minimap_y_edge(game->player.y, false, game->height, y_start);
 	draw_minimap_base(game, x_start, y_start, y_end);
 	draw_pixels(game, PLAYER, (game->player.x - 0.5 - x_start) * TILE,
 		(game->player.y - 0.5 - y_start) * TILE);
--- src_bonus/render_minimap_bonus.c
+++ src_bonus/render_minimap_bonus.c
@@ -3,31 +3,42 @@
 /*This function calculates the starting and end points of the portion
 of the minimap that we render to be visible at the moment, focusing on
 the player.*/
-static int	minimap_edge(double coordinate, int start, int max, int start_point)
+static int	minimap_x_edge(double player, bool start, int max, int start_point)
 {
 	int	point;
 
-	if (start == 0 || start == 2)
+	if (start)
 	{
-		point = coordinate - 10;
+		point = player - 10;
 		if (point < 0)
 			point = 0;
-		printf("coord %f point %d max %d\n", coordinate, point, max);
-		if (start == 0 && (point + 20) > max)
-			point -= point + 20 - max;
 	}
 	else
 	{
-		point = coordinate + 10;
-		if (start == 1 && point - start_point < 20)
+		point = player + 10;
+		if (point - start_point < 20)
 			point += (20 - (point - start_point));
-		if (start == 1 && point - start_point > 20)
-			point -= (point - start_point) - 20;
-		if (start == 3 && point - start_point < 12)
+		if (point >= max)
+			point = max;
+	}
+	return (point);
+}
+
+static int	minimap_y_edge(double player, int start, int max, int start_point)
+{
+	int	point;
+
+	if (start)
+	{
+		point = player - 6;
+		if (point < 0)
+			point = 0;
+	}
+	else
+	{
+		point = player + 6;
+		if (point - start_point < 12)
 			point += (12 - (point - start_point));
-		if (start == 3 && point - start_point > 12)
-			point -= (point - start_point) - 12;
-		printf("point %d max %d\n", point, max);
 		if (point >= max)
 			point = max;
 	}
@@ -47,8 +58,7 @@ static void	draw_minimap_base(t_game *game, int x_start, int y_start, int y_end)
 	while (y < y_end)
 	{
 		x = x_start;
-		x_end = minimap_edge(game->player.x, 1, ft_strlen(game->map[y]), x_start);
-		printf("x start %d end %d y start %d end %d\n", x_start, x_end, y_start, y_end);
+		x_end = minimap_x_edge(game->player.x, false, ft_strlen(game->map[y]), x_start);
 		x1 = 0;
 		while (x < x_end)
 		{
@@ -70,15 +80,9 @@ void	render_minimap(t_game *game)
 	int	y_start;
 	int	y_end;
 
-	printf("player x %f y %f\n", game->player.x, game->player.y);
-	x_start = minimap_edge(game->player.x, 0, game->width, 0);
-	y_start = minimap_edge(game->player.y, 2, game->height, 0);
-	y_end = minimap_edge(game->player.y, 3, game->height, y_start);
-	if (y_end - game->player.y < 2 && (y_start + 2 + 12 < game->height)) //this last condition doesnt work!! check simple map bottom
-	{
-		y_start += 2;
-		y_end = minimap_edge(game->player.y, 3, game->height, y_start);
-	}
+	x_start = minimap_x_edge(game->player.x, true, game->width, 0);
+	y_start = minimap_y_edge(game->player.y, true, game->height, 0);
+	y_end = minimap_y_edge(game->player.y, false, game->height, y_start);
 	draw_minimap_base(game, x_start, y_start, y_end);
 	draw_pixels(game, PLAYER, (game->player.x - 0.5 - x_start) * TILE,
 		(game->player.y - 0.5 - y_start) * TILE);

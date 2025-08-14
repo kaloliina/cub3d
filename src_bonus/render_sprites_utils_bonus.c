e02616fc206ecff6e5d02bbf3a1145878db1af71
sojala <sojala@student.hive.fi>
2025-08-14 17:26:54 +0300

    Had some unfinished comments in render sprites utils

--- src_bonus/render_sprites_utils_bonus.c
+++ src_bonus/render_sprites_utils_bonus.c
@@ -6,14 +6,14 @@
 /*   By: sojala <sojala@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 12:02:57 by sojala            #+#    #+#             */
-/*   Updated: 2025/08/14 17:11:01 by sojala           ###   ########.fr       */
+/*   Updated: 2025/08/14 17:24:47 by sojala           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3D_bonus.h"
 
-/*Because we only use this distance to sort the sprites,
-we will save some time and energy by not calculating the square root
+/*This distance is only used to sort the sprites, so to save some
+time and energy, the square root will not be calculated
 in this Pythagorean formula*/
 static double	find_sprite_dist(t_game *game, t_dda *dda, int i)
 {
@@ -23,8 +23,8 @@ static double	find_sprite_dist(t_game *game, t_dda *dda, int i)
 		* (dda->pos_y - game->sprites[i].y));
 }
 
-/*We first sort the sprites, calculating the distance from the player,
-so we'll draw the one furthest away first and the closest one last.*/
+/*Sorts the sprites so they will be drawn from furthest to
+closest, calculating distance from the player.*/
 void	sort_sprites(t_game *game, t_dda *dda, int i)
 {
 	int			j;
@@ -53,7 +53,7 @@ void	sort_sprites(t_game *game, t_dda *dda, int i)
 	}
 }
 
-/*Here we calculate the starting and ending points of the line
+/*Calculates the starting and ending points of the line
 that draws the sprite.
 
 We scale the y start and end points with the size of sprite texture,
